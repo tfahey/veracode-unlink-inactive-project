50715be9b19071a70acd86d7b174e656233804d4
tfahey <tntfahey@gmail.com>
2024-06-19 13:41:42 -0400

    Refactored and added "link" action

--- UnlinkInactiveProject.py
+++ UnlinkInactiveProject.py
@@ -7,6 +7,7 @@ import anticrlf
 import timeit
 import csv
 
+import requests.exceptions
 from veracode_api_py.api import VeracodeAPI as vapi, Applications, SCAApplications
 from veracode_api_py.constants import Constants
 
@@ -37,11 +38,56 @@ def logprint(log_msg):
     print(log_msg)
 
 
+def unlink_project(answer, app_guid, application_name, apps_skipped, apps_unlinked, project_guid, project_name):
+    # answer == "x" means stop prompting
+    if answer != "x":
+        answer = input(
+            "About to unlink SCA project " + project_name + " from Application " + application_name + " Are you sure?y/n/x:")
+        print("you answered " + answer)
+    if live_mode and (answer == "y" or answer == "x"):
+        logprint("Unlinking SCA project " + project_name + " from Application " + application_name)
+        logprint("Unlinking SCA project guid " + project_guid + " from Application " + app_guid)
+        try:
+            SCAApplications().unlink_project(app_guid, project_guid)
+            apps_unlinked = apps_unlinked + 1
+        except requests.exceptions.RequestException:
+            logprint(
+                "An error was encountered trying to unlink SCA project " + project_name + " from application " + application_name)
+        logprint("Running in live mode - this project has been unlinked")
+    else:
+        logprint("Skipping Unlinking SCA project " + project_name + " from Application " + application_name)
+        apps_skipped = apps_skipped + 1
+    return apps_skipped, apps_unlinked
+
+
+def link_project(answer, app_guid, application_name, apps_skipped, apps_linked, project_guid, project_name):
+    # answer == "x" means stop prompting
+    if answer != "x":
+        answer = input(
+            "About to link SCA project " + project_name + " to Application " + application_name + " Are you sure?y/n/x:")
+        print("you answered " + answer)
+    if live_mode and (answer == "y" or answer == "x"):
+        logprint("Linking SCA project " + project_name + " to Application " + application_name)
+        logprint("Linking SCA project guid " + project_guid + " to Application " + app_guid)
+        try:
+            SCAApplications().link_project(app_guid, project_guid)
+            apps_linked = apps_linked + 1
+        except requests.exceptions.RequestException:
+            logprint(
+                "An error was encountered trying to link SCA project " + project_name + " to application " + application_name)
+        logprint("Running in live mode - this project has been linked")
+    else:
+        logprint("Skipping linking SCA project " + project_name + " to Application " + application_name)
+        apps_skipped = apps_skipped + 1
+    return apps_skipped, apps_linked
+
+
 deleted_apps = []
 apps_with_one_GUID = []
 apps_with_multiple_GUIDs = []
 updated_linked_apps = []
-live_mode = False
+live_mode = True
+action = "unlink"  # valid values are "link" and "unlink"
 # Insert your Veracode account ID here
 account_ID = "00000"
 
@@ -72,6 +118,7 @@ def main():
     apps_wrong_account = 0
     apps_not_found = 0
     apps_unlinked = 0
+    apps_linked = 0
     apps_skipped = 0
 
     loop_start_time = timeit.default_timer()
@@ -131,6 +178,7 @@ def main():
         legacy_id = int(this_app["APP_ID"])
         project_guid = this_app["project_guid"]
         project_name = this_app["project_name"]
+        app_guid = ""
 
         # Verify that the application profile is owned by the correct account ID.
         # If not, print and log a message, and break out of the loop for this application.
@@ -140,7 +188,7 @@ def main():
             apps_wrong_account = apps_wrong_account + 1
             continue
 
-        logprint("Calling Applications API for app name" + this_app["app_name"] + " with legacy id " + str(legacy_id))
+        logprint("Calling Applications API for app name " + this_app["app_name"] + " with legacy id " + str(legacy_id))
         app_info = Applications().get(legacy_id=legacy_id)
 
         # logprint(str(app_info))
@@ -154,7 +202,8 @@ def main():
             if key == "_embedded":
                 if len(app_info["_embedded"]["applications"]) != 1:
                     print("WARNING! There are " + str(len(app_info["_embedded"]["applications"])) + " apps for guid!")
-                logprint("The app guid is: " + app_info["_embedded"]["applications"][0]["guid"] + " and the project guid is: " + project_guid)
+                app_guid = app_info["_embedded"]["applications"][0]["guid"]
+                logprint("The app guid is: " + app_guid + " and the project guid is: " + project_guid)
                 # print("app_info: " + str(app_info["_embedded"]["applications"][0]))
 
                 # The Applications API found a matching application profile for this legacy_id
@@ -162,18 +211,14 @@ def main():
 
                 application_name = app_info["_embedded"]["applications"][0]["profile"]["name"]
 
-                # answer == "x" means stop prompting
-                if answer != "x":
-                    answer = input("About to unlink SCA project " + project_name + " from Application " + application_name + " Are you sure?y/n/x:")
-                    print("you answered " + answer)
-                if live_mode and (answer == "y" or answer == "x"):
-                    logprint("Unlinking SCA project " + project_name + " from Application " + application_name)
-                    logprint("Running in live mode - this project will be unlinked")
-                    apps_unlinked = apps_unlinked + 1
-                    # SCAApplications().unlink_project(app_info["guid"], project_guid)
+                if action == "unlink":
+                    apps_skipped, apps_unlinked = unlink_project(answer, app_guid, application_name, apps_skipped,
+                                                                 apps_unlinked, project_guid, project_name)
+                elif action == "link":
+                    apps_skipped, apps_linked = link_project(answer, app_guid, application_name, apps_skipped,
+                                                                 apps_linked, project_guid, project_name)
                 else:
-                    logprint("Skipping Unlinking SCA project " + project_name + " from Application " + application_name)
-                    apps_skipped = apps_skipped + 1
+                    logprint("Invalid action specified")
 
         if not application_found:
             logprint("Warning!! Applications API did not find an application with app name" + this_app["app_name"] + " with legacy id " + str(legacy_id))
@@ -186,8 +231,13 @@ def main():
     logprint(str(matchCount) + " applications profiles were matched by legacy ID")
     logprint(str(apps_wrong_account) + " applications were skipped because the account id was incorrect.")
     logprint(str(apps_not_found) + " applications were skipped because the Applications API did not find them.")
-    logprint(str(apps_unlinked) + " applications were successfully unlinked.")
-    logprint(str(apps_skipped) + " applications were skipped because the account id was incorrect.")
+    if action == "unlink":
+        logprint(str(apps_unlinked) + " applications were successfully unlinked.")
+    elif action == "link":
+        logprint(str(apps_linked) + " applications were successfully linked.")
+    else:
+        logprint("Invalid action specified")
+    logprint(str(apps_skipped) + " applications were skipped because the user answered 'n' or the script was in trial mode.")
     logprint("The double nested loop took " + str(loop_elapsed_time) + " seconds")
     logprint("The API calls loop took " + str(api_elapsed_time) + " seconds, or " + str(api_elapsed_time/60.) + " minutes")
     logprint('======== ending UnlinkInactiveProject.py run ========')
